7d068244dc7bd7bd93b1bcc1ee17f3a189bb748c
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-10-17 13:26:39 -0300

    feat/main: footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer>
+            <section className="footerContainer items-center justify-center h-40 px-20 flex w-full bg-custom-blue border-t border-black font-karla text-sm">
+                <div className="footerItem w-1/4">Item 1</div>
+                <div className="footerItem w-1/4">Item 2</div>
+                <div className="footerItem w-1/4 flex-col">
+                    <p><b>Facultad de Artes Universidad de Chile</b></p>
+                    <p>Dr. Sotero del Río 1270, 8340381 Santiago,</p>
+                    <p>Región Metropolitana, Chile</p>
+                </div>
+                <div className="footerItem w-1/4">Item 4</div>
+            </section>
+        </footer>
+    );
+}
+
+export default Footer;
