792be8c7af99ba4fae772ae52710812682c64be3
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-10-20 14:06:14 -0300

    feat/main: Cambios navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import logoImage from '@/assets/logo-chucaolab.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faYoutube, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 const Navbar = () => {
     return (
-        <nav className=" h-20 w-full flex items-center justify-between px-10 bg-transparent border-b border-gray-100 text-white shadow-lg font-jetbrains">
-            <div className="logo text-black"><Link href="/">ChucaoLab</Link></div>
+        <nav className="absolute z-50 h-[90px] w-full flex items-center justify-between px-20 bg-transparent border-b border-gray-100 text-white shadow-lg font-jetbrains">
+            <Link href="/"><Image src={logoImage} alt="ChucaoLab" className='h-[70px] w-[90px]' /></Link>
             <ul className="sections gap-20 flex text-black cursor-pointer">
                 <li className='relative group '><h2 className='text-black group-hover:text-blue-500'>INVESTIGACIÓN</h2>
                     <ul className='absolute hidden group-hover:block bg-white shadow-lg rounded pt-2 py-2 w-[200px] rounded-b cursor-pointer'>
@@ -38,10 +40,10 @@ const Navbar = () => {
                 <a className="socialMediaLink h-10 w-10 rounded-full p-2 bg-black hover:bg-gray-700" href="https://www.linkedin.com/company/chucaolab/" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faLinkedinIn} className='h-full w-full' />
                 </a>
-                <a className="socialMediaLink h-10 w-10 rounded-full p-2 bg-black hover:bg-gray-700" href="https://www.instagram.com/chucaolab/" target="_blank" rel="noopener noreferrer">
+                <a className="socialMediaLink h-10 w-10 rounded-full p-2 bg-black hover:bg-gray-700" href="https://www.instagram.com/chucao.lab/" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faInstagram} className='h-full w-full' />
                 </a>
-                <a className="socialMediaLink h-10 w-10 rounded-full p-2 bg-black hover:bg-gray-700" href="https://www.youtube.com/@chucaolab" target="_blank" rel="noopener noreferrer">
+                <a className="socialMediaLink h-10 w-10 rounded-full p-2 bg-black hover:bg-gray-700" href="https://www.youtube.com/@chucao_lab" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faYoutube} className='h-full w-full' />
                 </a>
             </div>
