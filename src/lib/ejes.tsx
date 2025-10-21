bd991090186dcd5e56776b21c0e1d1398e1b1bb8
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-10-21 11:42:35 -0300

    feat/main: ejes models

---
+++ src/lib/ejes.tsx
@@ -0,0 +1,22 @@
+interface ejes {
+    id: number;
+    titulo: string;
+
+}
+
+export const ejesChucao: ejes [] = [
+    {
+        id: 1,
+        titulo: "PAISAJE URBANO SONORO"
+    },
+      {
+        id: 2,
+        titulo: "ESTUDIOS DE BIODIVERSIDAD Y ESTADO DE ECOSISTEMAS"
+    },
+      {
+        id: 3,
+        titulo: "CREACIÓN ARTÍSTICA"
+    },
+
+
+]
