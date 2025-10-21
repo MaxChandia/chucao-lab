ab13e902b98266afaf9f44b05f183c2306b1640a
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-10-21 15:17:08 -0300

    feat/main: cambiosn landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
                   </p>
               </div>
               <Link href="/quienes-somos">
-                  <button className="h-12 w-36 p-1 rounded-[30px] bg-black text-white font-jetbrains text-sm">
+                  <button className="h-12 w-36 p-1 rounded-[30px] bg-black text-white font-jetbrains text-sm hover:bg-gray-700 flex items-center justify-center gap-2">
                       QUIÉNES SOMOS
                   </button>
               </Link>
@@ -37,14 +37,17 @@ export default function Home() {
       </section>
       <section className="ejes h-[50vh] w-full flex flex-col mt-20 px-20">
         <div className="sectionHeader flex justify-between items-center gap-4 mb-10 border-b-2 border-dotted border-black pb-2">
-          <h3 className="text-sm font-jetbrains">EJES DE INVESTIGACIÓN</h3>
+          <h3 className="text-sm font-jetbrains">EJES TEMÁTICOS</h3>
         </div>
-        <div className="newsContainer flex gap-10">
+        <div className="newsContainer flex justify-center gap-20">
           {ejesChucao.slice(0,3).map((ejes)=>(
-            <div key={ejes.id} className="newsItem w-1/3 " >
-              <span className="rounded-sm h-60 w-80 bg-gray-300 inline-block"></span>
-              <div>
-                <h2 className="font-bold font-jetbrains">{ejes.titulo}</h2>
+            <div key={ejes.id} className="newsItem w-1/3 gap-5" >
+              <span className="rounded-sm h-60 w-full bg-gray-300 inline-block"></span>
+              <div className="flex gap-2 mt-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="bi bi-soundwave shrink-0" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5m-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5m12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5"/>
+                </svg>
+                <h2 className="font-bold font-jetbrains text-left text-lg leading-tight">{ejes.titulo}</h2>
               </div>
             </div>
           ))}
