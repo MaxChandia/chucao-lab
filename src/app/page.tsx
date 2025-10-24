d4913eeb9a0896b065cee7c41c3f7997ba76d576
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-10-24 13:03:22 -0300

    feat\main: cambions landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           <div className="px-20 flex flex-col gap-20 w-full absolute top-[40%]">
               <div className="textHero flex flex-col">
                   <h1 className="text-2xl font-bold">
-                      ChucaoLab es un laboratorio dedicado al estudio del Paisaje Sonoro
+                      ChucaoLab es un laboratorio interdisciplinario de gestión, diseño y creación del paisaje sonoro ambiental
                   </h1>
                   <p className="text-2xl w-1/2">
                       Nos enfocamos en la investigación, la enseñanza y la conexión activa con la comunidad
@@ -41,15 +41,15 @@ export default function Home() {
         </div>
         <div className="newsContainer flex justify-center gap-20">
           {ejesChucao.slice(0,3).map((ejes)=>(
-            <div key={ejes.id} className="newsItem w-1/3 gap-5" >
-              <span className="rounded-sm h-60 w-full bg-gray-300 inline-block"></span>
+            <Link key={ejes.id} href={ejes.url} className="ejeItem w-1/3 cursor-pointer hover:scale-101 transition-transform duration-300">
+              <Image src={ejes.image_url} alt={ejes.titulo} className="rounded-md" width={400} height={240} />
               <div className="flex gap-2 mt-3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="bi bi-soundwave shrink-0" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5m-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5m12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5"/>
                 </svg>
                 <h2 className="font-bold font-jetbrains text-left text-lg leading-tight">{ejes.titulo}</h2>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </section>
