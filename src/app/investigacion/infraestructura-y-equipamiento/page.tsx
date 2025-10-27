8a6d4fcc15533dc70765b431b46a8acd3a4fc5c3
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-10-27 11:35:24 -0300

    feat/main: investigacion pages

---
+++ src/app/investigacion/infraestructura-y-equipamiento/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import heroImage from '@/assets/hero_sections.webp';
+
+const InfraestructuraYEquipamiento = () => {
+
+    return (
+        <div>
+            <section className="hero h-[60vh] sm:h-[70vh] md:h-[80vh] w-full font-karla relative">
+                <Image
+                    src={heroImage}
+                    alt="Hero Section"
+                    fill
+                    className="object-cover z-0"
+                />
+                <div className="absolute inset-0 flex items-center justify-center z-10">
+                    <h2 className="lg:text-5xl md:text-3xl font-bold text-black">
+                        Infraestructura y Equipamiento
+                    </h2>
+                </div>
+                <span className="absolute bottom-0 left-0 w-full h-5 bg-sage-green border-y-2 border-black z-10"></span>
+            </section>
+        </div>
+    );
+
+};
+
+export default InfraestructuraYEquipamiento;
