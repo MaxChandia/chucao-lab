520fd218e840ef25d349ce9796a68a66e9abce72
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-10-27 16:20:47 -0300

    feat/main: quienes-somos page

--- src/app/quienes-somos/page.tsx
+++ src/app/quienes-somos/page.tsx
@@ -4,7 +4,7 @@ import { faEnvelope, faArrowRight, faArrowLeft } from "@fortawesome/free-solid-s
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import Image from "next/image";
-import heroImage from '@/assets/hero-sections.png';
+import heroImage from '@/assets/hero_sections.webp';
 import { equipo } from "@/lib/equipo";
 
 const QuienesSomos = () => {
@@ -26,25 +26,23 @@ const QuienesSomos = () => {
 
   return (
     <div>
-      <section className="hero h-[80vh] w-full font-karla">
-        <div className="relative h-full w-full">
-             <Image
+        <section className="hero h-[60vh] sm:h-[70vh] md:h-[80vh] w-full font-karla relative">
+            <Image
                 src={heroImage}
                 alt="Hero Section"
                 fill
-                className="object-cover"
+                className="object-cover z-0"
             />
-        </div>
-           
-        <div className="absolute flex items-center justify-center top-[40%] left-[40%] ">
-            <h2 className="text-5xl font-bold text-black font-karla"><b>Quiénes Somos</b></h2>
-        </div>
-        <span className="h-5 w-full bg-sage-green border-y-2 border-black inline-block"></span>
-
+            <div className="absolute inset-0 flex items-center justify-center z-10">
+                <h2 className="lg:text-5xl text-3xl font-bold text-black">
+                    Quiénes Somos
+                </h2>
+            </div>
+            <span className="absolute bottom-0 left-0 w-full h-5 bg-sage-green border-y-2 border-black z-10"></span>
         </section>
-      <section className="px-20 h-[70vh] w-full flex items-center justify-center gap-6 font-karla">
-        <div className="px-5 w-2/4 h-full flex flex-col  justify-center leading-relaxed">
-            <p className="mt-4 line-height-5">En <b>ChucaoLab</b> exploramos el Paisaje Sonoro como una forma de entender y conectar con nuestro entorno. </p>
+      <section className="px-10 py-16 lg:px-20 min-h-[70vh] w-full flex flex-col lg:flex-row items-center justify-center gap-6 font-karla">
+        <div className="px-5 w-full lg:w-2/4 h-full flex flex-col  justify-center leading-relaxed text-justify lg:text-lef gap-4 lg:gap-0">
+            <p className="lg:text-md text-base mt-4 line-height-5">En <b>ChucaoLab</b> exploramos el Paisaje Sonoro como una forma de entender y conectar con nuestro entorno. </p>
             <p> Somos un laboratorio dedicado a la investigación, enseñanza y difusión de los sonidos naturales en entornos urbanos, inspirados por el Chucao, un ave nativa de Chile que simboliza la riqueza acústica de la naturaleza. </p>
             <p>Nuestro trabajo busca fomentar el bienestar y la conciencia ambiental a través del sonido.</p>
             <p className="mt-10 italic">ChucaoLab es un laboratorio de Paisaje Sonoro situado en la Facultad de Artes, perteneciente al Departamento de Sonido de la Universidad de Chile. </p>
@@ -53,16 +51,17 @@ const QuienesSomos = () => {
             <span className="w-[700px] h-[300px] rounded-[20px] bg-gray-300 inline-block"></span>
         </div>
       </section>
-      <section className="px-20 h-[70vh] flex flex-col justify-center items-center font-karla">
+      {/* Equipo Section */}
+      <section className="px-4 sm:px-10 lg:px-20 py-16 min-h-screen lg:min-h-[70vh] flex flex-col justify-center items-center font-karla">
         <div className="sectionHeader w-full flex justify-between items-center gap-4 mb-10 border-b-2 border-dotted border-black pb-2">
           <h3 className="text-xl font-bold font-jetbrains">EQUIPO</h3>
-          <div className="h-12 w-20 gap-8 flex">
+          <div className="h-5 lg:h-10 w-20 gap-8 flex">
             <FontAwesomeIcon icon={faArrowLeft} onClick={handlePrev} className="text-black cursor-pointer"/>
             <FontAwesomeIcon icon={faArrowRight} onClick={handleNext} className="text-black cursor-pointer" />
             </div>
         </div>
-        <div className="h-[600px] w-full">
-            <ul className="mt-10 px-5 flex items-center justify-center gap-[120px] flex-wrap">
+        <div className="w-full">
+            <ul className="mt-10 px-5 flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-[120px] flex-wrap">
                 {equipo.slice(startIndex, startIndex + membersPerPage).map((miembro) => (
                     <li key={miembro.id} className="flex flex-col items-center justify-center font-karla gap-5">
                         <span className="inline-block h-[130px] w-[130px] rounded-full bg-gray-300"></span>
