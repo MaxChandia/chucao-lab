c46b2a48cd6828727ad152dca788c0ef81bbf1f0
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-11-05 10:14:05 -0300

    fix/main: Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
           <span className="absolute bottom-0 left-0 w-full h-5 bg-sage-green border-y-2 border-black z-10"></span>
       </section>
       {/*Ejes temáticos */}
-      <section className="ejes min-h-screen lg:min-h-[50vh] w-full flex flex-col mt-20 px-10 lg:px-20">
+      <section className="ejes sm:h-[100vh] lg:h-[50vh] w-full flex flex-col mt-20 px-10 lg:px-20">
         <div className="sectionHeader flex justify-between items-center gap-4 mb-10 border-b-2 border-dotted border-black pb-2">
           <h3 className="text-sm font-jetbrains">EJES TEMÁTICOS</h3>
         </div>
@@ -80,9 +80,9 @@ export default function Home() {
         </div>
       </section>
       <section className="news min-h-screen lg:min-h-[70vh] w-full flex flex-col mt-20 px-10 py-10 lg:px-20">
-        <div className="sectionHeader flex justify-between items-center gap-4 mb-10 border-b-2 border-dotted border-black pb-2">
+        <div className="sectionHeader flex justify-between items-center  gap-4 mb-10 border-b-2 border-dotted border-black pb-2">
           <h3 className="text-md font-jetbrains">NOVEDADES</h3>
-          <div className="h-10 w-20 gap-8 flex">
+          <div className="h-5 w-20 gap-8 flex">
             <FontAwesomeIcon icon={faArrowLeft} className="text-black cursor-pointer"/>
             <FontAwesomeIcon icon={faArrowRight} className="text-black cursor-pointer" />
           </div>
@@ -91,7 +91,7 @@ export default function Home() {
           {noticias.slice(0, 3).map((noticia) => (
             <Link 
               key={noticia._id} 
-              href={`/vinculacion/noticias/${noticia.slug.current}`}  // ← Usa slug.current
+              href={`/vinculacion/noticias/${noticia.slug.current}`} 
               className="newsItem min-h-[480px] sm:min-h-[420px] lg:min-h-[480px] flex flex-col w-full lg:w-1/3 cursor-pointer hover:scale-101 transition-transform duration-300"
             >
               {noticia.imagenDestacadaUrl && (
@@ -103,8 +103,8 @@ export default function Home() {
                   className="rounded-md"
                 />
               )}
-              <div className="mt-3 flex flex-col flex-1 p-3">
-                <div className="flex gap-2 min-h-[60px]">
+              <div className="mt-3 flex flex-col flex-1 py-3">
+                <div className="flex gap-2 min-h-[60px] lg:w-[400px]">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="bi bi-soundwave shrink-0" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5m-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5m12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5"/>
                   </svg>
@@ -112,8 +112,8 @@ export default function Home() {
                     {noticia.titulo}
                   </h2>
                 </div>
-                <div className="mt-1 text-justify lg:text-justify text-sm font-karla">
-                  {noticia.bajada}  {/* ← Simplificado: es un string */}
+                <div className="mt-1 text-justify lg:text-justify text-sm font-karla lg:w-[400px] flex-1">
+                  {noticia.bajada} 
                 </div>
               </div>
             </Link>
