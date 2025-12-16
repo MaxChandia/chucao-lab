432dcf3ae1ea2ec20ae018589389cc3b39a22736
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-12-16 01:40:38 -0300

    feat/main: Última actualización cursos-page

--- src/app/docencia/cursos/page.tsx
+++ src/app/docencia/cursos/page.tsx
@@ -8,8 +8,8 @@ export default async function CursosPage() {
     const cursos = await sanityService.getAllCursos();
 
     return (
-        <div className="min-h-screen bg-white">
-            {/* HERO SECTION (Se mantiene igual) */}
+        <div className="min-h-screen">
+            {/* HERO SECTION*/}
             <section className="hero h-[60vh] sm:h-[70vh] md:h-[80vh] w-full font-karla relative">
                 <Image
                     src={heroImage}
@@ -26,9 +26,9 @@ export default async function CursosPage() {
                 <span className="absolute bottom-0 left-0 w-full h-5 bg-sage-green border-y-2 border-black z-10"></span>
             </section>
 
-            {/* LISTADO TIPO RECTÁNGULO */}
-            <section className="max-w-5xl mx-auto px-4 py-20 font-karla">
-                <div className="flex flex-col gap-8">
+   
+            <section className="mx-auto px-4 py-20 font-karla items-center flex flex-col">
+                <div className="flexflex-col w-3/4 gap-8">
                     {cursos?.length > 0 ? (
                         cursos.map((curso: Curso) => (
                             <Link 
@@ -36,7 +36,7 @@ export default async function CursosPage() {
                                 href={`/docencia/cursos/${curso.slug.current}`}
                                 className="group flex flex-col md:flex-row bg-white border border-gray-200 rounded-2xl overflow-hidden hover:shadow-xl transition-all duration-300 h-auto md:h-72"
                             >
-                                {/* LADO IZQUIERDO: TEXTO (Título y Resumen) */}
+                            
                                 <div className="flex-1 p-8 flex flex-col justify-center items-start order-2 md:order-1">
                                     <h3 className="text-2xl lg:text-3xl font-bold font-jetbrains mb-4 group-hover:text-sage-green transition-colors">
                                         {curso.titulo}
@@ -51,7 +51,6 @@ export default async function CursosPage() {
                                     </span>
                                 </div>
 
-                                {/* LADO DERECHO: IMAGEN */}
                                 <div className="relative w-full md:w-2/5 lg:w-1/3 h-64 md:h-full order-1 md:order-2 overflow-hidden">
                                     {curso.imagenPrincipal?.asset?.url ? (
                                         <Image
