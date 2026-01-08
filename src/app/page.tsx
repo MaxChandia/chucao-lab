d41d5030a16e93f2711072221eadaa0276a20912
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-01-08 10:56:12 -0300

    feat/main: acctualización pagelanding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import Image from "next/image";
 import heroImage from '@/assets/hero-landing.png';
-import { ejesChucao } from "@/lib/ejes";
 import {Eje, Noticia, ProyectoPrincipal, SeccionHero} from "@/lib/sanityClasses";
 import { sanityService } from "@/lib/sanityService";
+import NewsSlider from "@/components/newsSlider";
 
 
 
@@ -100,56 +98,7 @@ export default async function Home() {
                     </div>
                 </Link>
             </section>
-      <section className="news min-h-screen lg:min-h-[70vh] w-full flex flex-col mt-20 px-10 py-10 lg:px-20">
-        <div className="sectionHeader flex justify-between items-center  gap-4 mb-10 border-b-2 border-dotted border-black pb-2">
-          <h3 className="text-md font-jetbrains">NOVEDADES</h3>
-          <div className="h-5 w-20 gap-8 flex">
-            <FontAwesomeIcon icon={faArrowLeft} className="text-black cursor-pointer"/>
-            <FontAwesomeIcon icon={faArrowRight} className="text-black cursor-pointer" />
-          </div>
-        </div>
-        <div className="newsContainer flex flex-col lg:flex-row gap-20">
-          {noticias.slice(0, 3).map((noticia) => (
-            <Link 
-              key={noticia._id} 
-              href={`/vinculacion/noticias/${noticia.slug.current}`} 
-              className="newsItem min-h-[480px] sm:min-h-[420px] lg:min-h-[480px] flex flex-col w-full lg:w-1/3 cursor-pointer hover:scale-101 transition-transform duration-300"
-            >
-              <div className=" transition-all duration-300 transform hover:-translate-y-2 hover:text-gray-500 ">
-              {noticia.imagenDestacadaUrl && (
-                <div className="relative">
-                <Image 
-                  src={noticia.imagenDestacadaUrl}
-                  alt={noticia.titulo} 
-                  width={400} 
-                  height={250} 
-                  className="rounded-md"
-                />
-                <div className="absolute top-4 left-4">
-                                                <span className="px-4 py-1.5 rounded-full text-xs font-semibold bg-white text-black">
-                                                  {noticia.categoria}
-                                                </span>
-                                            </div>
-                </div>
-              )}
-              <div className="mt-3 flex flex-col flex-1 py-3">
-                <div className="flex gap-2 min-h-[60px] lg:w-[400px]">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="bi bi-soundwave shrink-0" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5m-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5m12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5"/>
-                  </svg>
-                  <h2 className="font-bold font-jetbrains text-justify lg:text-left text-sm leading-tight">
-                    {noticia.titulo}
-                  </h2>
-                </div>
-                <div className="mt-1 text-justify lg:text-justify text-sm font-karla lg:w-[400px] flex-1">
-                  {noticia.bajada} 
-                </div>
-              </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </section>
+      <NewsSlider noticias={noticias} />
       
     </div>
   );
