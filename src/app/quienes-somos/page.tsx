12559d5ca61404eb6b448c74c01e16a6c6f20661
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-01-12 16:43:11 -0300

    feat/main: actualizacion quinessomos  page

--- src/app/quienes-somos/page.tsx
+++ src/app/quienes-somos/page.tsx
@@ -1,8 +1,9 @@
 import { sanityService } from "@/lib/sanityService";
 import Image from "next/image";
 import heroImage from '@/assets/hero_sections.webp'
-import { Colaborador, SeccionInformativa, SeccionSobreNosotros } from "@/lib/sanityClasses";
-import CarrouselItem from "@/lib/carrouselItem";
+import {SeccionInformativa} from "@/lib/types/pages";
+import {Colaborador} from "@/lib/types/miembros";
+import CarrouselItem from "@/lib/carrouselEquipo";
 import { PortableText } from "next-sanity";
 import PortableTextComponents from "@/lib/portableTextComponents";
 
@@ -28,19 +29,30 @@ export default async function QuienesSomos() {
         <span className="absolute bottom-0 left-0 w-full h-5 bg-sage-green border-y-2 border-black z-10"></span>
       </section>
       
-      <section className="px-10 py-2 lg:px-20 min-h-[70vh] w-full flex flex-col lg:flex-row items-center justify-center gap-6 font-karla">
-        <div className="px-5 w-full lg:w-2/4 h-full flex flex-col justify-center leading-relaxed text-justify lg:text-lef gap-4 lg:gap-0">
-          <PortableText value={quienesSomos.contenido} components={PortableTextComponents} />
-        </div>
-        <div className="chucaoImg w-2/3 h-full flex items-center justify-center">
-         <Image
-            src={quienesSomos.imagenDestacada?.asset.url || ''}
-            alt={quienesSomos.imagenDestacada?.alt || 'Imagen'}
-            width={500}
-            height={250}
-            className="rounded-lg shadow-lg"
-          />
-        </div>
+       <section className="relative flex flex-col items-center justify-center w-full px-10 lg:px-[100px] py-20 font-karla gap-5">
+          <div className="flex flex-col lg:flex-row justify-between prose prose-lg w-full text-gray-700 leading-relaxed">
+              <div className="textContainer flex flex-col justify-center lg:w-1/2 lg:pr-20">
+                  <PortableText value={quienesSomos.contenido} components={PortableTextComponents} />
+                  <div className="flex justify-center lg:justify-start items-center gap-4 mb-8">
+                      <div className="w-16 h-1 bg-sage-green"></div>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="text-sage-green" viewBox="0 0 16 16">
+                              <path fillRule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5m-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5m12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5"/>
+                          </svg>
+                      <div className="w-16 h-1 bg-sage-green"></div>
+                  </div>
+              </div>
+              <div className="ImageContainer flex lg:justify-end lg:w-1/2">
+                  {quienesSomos.imagenDestacada?.asset?.url && (
+                      <Image
+                          src={quienesSomos.imagenDestacada.asset.url}
+                          alt={quienesSomos.imagenDestacada.alt || 'Imagen descriptiva'}
+                          width={800}
+                          height={400}
+                          className="rounded-md"
+                      />
+                  )}
+              </div>
+          </div>
       </section>
 
       <CarrouselItem equipo={equipo} />
