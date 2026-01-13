8b3ba5cc5a731955028418cfe474021ae73d78b6
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-01-12 23:12:53 -0300

    feat/main: actualizacion footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,38 +1,36 @@
-import artesLogo from '@/assets/artes.png';
-import dsonLogo from '@/assets/dson.png';
-import anidLogo from '@/assets/anidLogo.png';
-import uchileLogo from '@/assets/uchileLogo.png';
 import Image from 'next/image';
+import { sanityService } from '@/lib/sanityService';
 
-const Footer = () => {
+const Footer = async () => {
+    
+  const data = await sanityService.getAllLogosFooter();
+  if (!data?.logos || data.logos.length === 0) {
     return (
-        <footer className="w-full">
-          
-            <section className="footerContainer flex flex-col lg:flex-row items-center justify-center w-full bg-transparent border-t border-black font-karla text-sm gap-10 lg:gap-0 py-10 lg:py-14 px-4 lg:px-[100px]">
-                
-              
-                <div className="footerItem w-full lg:w-1/4 flex items-center justify-center">
-                    <Image src={anidLogo} alt="ANID" className='w-[180px] h-[130px] object-contain' />
-                </div>
-
-                
-                <div className="footerItem w-full lg:w-1/4 flex items-center justify-center">
-                    <Image src={uchileLogo} alt="Universidad de Chile" className='w-[150px] h-[130px] object-contain' />
-                </div>
-
-               
-                <div className="footerItem w-full lg:w-1/4 flex items-center justify-center">
-                    <Image src={artesLogo} alt="Facultad de Artes" className='w-[300px] h-[100px] object-contain' />
-                </div>
-
-                
-                <div className="footerItem w-full lg:w-1/4 flex items-center justify-center">
-                    <Image src={dsonLogo} alt="Departamento de Sonido" className='w-[250px] object-contain' />
-                </div>
-                
-            </section>
-        </footer>
+      <footer className="w-full">
+        <section className="footerContainer flex items-center justify-center w-full border-t border-black py-10">
+          <p>Inserte Logos en Sanity</p>
+        </section>
+      </footer>
     );
+  }
+
+  return (
+    <footer className="w-full">
+      <section className="footerContainer flex flex-col lg:flex-row items-center justify-center w-full bg-transparent border-t border-black font-karla text-sm gap-10 lg:gap-0 py-10 lg:py-14 px-4 lg:px-[100px]">
+        {data.logos.map((logo: any) => (
+          <div key={logo._key} className="footerItem w-full lg:w-1/4 flex items-center justify-center">
+            <Image 
+              src={logo.url} 
+              alt={logo.alt || "Logo colaborador"} 
+              width={300}    
+              height={130}
+              className="object-contain max-h-[130px] w-auto" 
+            />
+          </div>
+        ))}
+      </section>
+    </footer>
+  );
 }
 
 export default Footer;
