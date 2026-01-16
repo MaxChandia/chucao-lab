13337304a37f806b0340f0aaddb9001748462603
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-01-16 02:03:29 -0300

    feat/main: actualización proyectos page

--- src/app/investigacion/proyectos/page.tsx
+++ src/app/investigacion/proyectos/page.tsx
@@ -23,7 +23,7 @@ const Proyectos = async () => {
                 </div>
                 <span className="absolute bottom-0 left-0 w-full h-5 bg-sage-green border-y-2 border-black z-10"></span>
             </section>
-            <section className="min-h-screen p-20">
+            <section className="h-auto p-20">
                 <div className=" gap-10">
                     {proyectos.map((proyecto) => (
                         <div 
@@ -31,10 +31,17 @@ const Proyectos = async () => {
                             className="border border-gray-300 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300"
                         >
                             <a href={`/investigacion/proyectos/${proyecto.slug.current}`}>
-                                <div className="p-6">
-                                    <h3 className="text-xl font-bold mb-4">{proyecto.titulo}</h3>
+                                <div className="relative w-full h-64 lg:h-80">
+                                    <Image
+                                        src={proyecto.imagenDestacada?.asset?.url || ''}
+                                        alt={proyecto.titulo}
+                                        fill
+                                        className="object-cover h-64 w-full"
+                                    />
+                                    <div className="absolute inset-0 bg-black bg-opacity-10 flex items-center justify-center hover:bg-opacity-20 transition duration-300">
+                                        <h3 className="text-xl lg:text-3xl font-bold text-black text-center px-4">{proyecto.titulo}</h3>
+                                    </div>
                                 </div>
-
                             </a>
                         </div>
                     ))}
