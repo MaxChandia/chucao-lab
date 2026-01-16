b119d15c31059b62bfac5804e4ede7857f11839e
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-01-16 02:02:39 -0300

    feat/main: actualizacion footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import { sanityService } from '@/lib/sanityService';
+import { FooterData, FooterLogo } from '@/lib/types/footer';
 
 const Footer = async () => {
-    
-  const data = await sanityService.getAllLogosFooter();
+  const data: FooterData = await sanityService.getAllLogosFooter();
+
   if (!data?.logos || data.logos.length === 0) {
     return (
       <footer className="w-full">
@@ -16,15 +17,18 @@ const Footer = async () => {
 
   return (
     <footer className="w-full">
-      <section className="footerContainer flex flex-col lg:flex-row items-center justify-center w-full bg-transparent border-t border-black font-karla text-sm gap-10 lg:gap-0 py-10 lg:py-14 px-4 lg:px-[100px]">
-        {data.logos.map((logo: any) => (
-          <div key={logo._key} className="footerItem w-full lg:w-1/4 flex items-center justify-center">
+      <section className="footerContainer flex flex-col lg:flex-row items-center justify-center w-full bg-transparent border-t border-black font-karla text-sm gap-10 lg:gap-[100px] py-10 lg:py-14 px-4 lg:px-[100px]">
+        {data.logos.map((logo: FooterLogo) => (
+          <div 
+            key={logo._key} 
+            className="footerItem flex items-center justify-center w-[150px] h-[150px] lg:w-[200px] lg:h-[120px] relative"
+          >
             <Image 
               src={logo.url} 
               alt={logo.alt || "Logo colaborador"} 
-              width={300}    
-              height={130}
-              className="object-contain max-h-[130px] w-auto" 
+              fill 
+              sizes="(max-width: 768px)"
+              className="object-contain" 
             />
           </div>
         ))}
