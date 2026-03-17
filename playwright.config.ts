e511f2f0976078b1df4d731bc7731bb835093cbe
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-03-17 00:57:10 -0300

    fix: playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,9 +70,11 @@ export default defineConfig({
     // },
   ],
 
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
+webServer: {
+  command: 'npm run build && npm run start',  
+  url: 'http://localhost:3000',
+  reuseExistingServer: !process.env.CI,
+  timeout: 120 * 1000, 
+},
+  
 });
