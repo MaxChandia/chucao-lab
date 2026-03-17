2563b61575566f40704d2de9cc3449fc9995c3d7
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-03-16 23:44:48 -0300

    feat: agregar pop-up al compartir

--- src/components/share-buttons/shareButtons.tsx
+++ src/components/share-buttons/shareButtons.tsx
@@ -16,16 +16,16 @@ const ShareButtons = ({title}:{title:string}) => {
    <div className="px-5">
       <h2 className="font-bold mr-2">Compartir en:</h2>
       <div className="flex gap-4 mt-2">
-        <a href={`https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`} aria-label="Compartir en Facebook" >
+        <a href={`https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`} target="_blank" rel="noopener noreferrer" aria-label="Compartir en Facebook" >
           <FontAwesomeIcon icon={faFacebook} className="text-[25px]"/>
         </a>
-        <a href={`https://twitter.com/intent/tweet?url=${currentUrl}&text=${title}`} aria-label="Compartir en Twitter">
+        <a href={`https://twitter.com/intent/tweet?url=${currentUrl}&text=${title}`} target="_blank" rel="noopener noreferrer" aria-label="Compartir en Twitter">
           <FontAwesomeIcon icon={faXTwitter} className="text-[25px]"/>
         </a>
-        <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${currentUrl}`} aria-label="Compartir en LinkedIn">
+        <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${currentUrl}`} target="_blank" rel="noopener noreferrer" aria-label="Compartir en LinkedIn">
           <FontAwesomeIcon icon={faLinkedin} className="text-[25px]"/>
         </a>
-        <a href={`https://api.whatsapp.com/send?text=${title} ${currentUrl}`} aria-label="Compartir en WhatsApp">
+        <a href={`https://api.whatsapp.com/send?text=${title} ${currentUrl}`} target="_blank" rel="noopener noreferrer" aria-label="Compartir en WhatsApp">
           <FontAwesomeIcon icon={faWhatsapp} className="text-[25px]"/>
         </a>
       </div>
