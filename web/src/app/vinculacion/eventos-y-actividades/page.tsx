66cf2534f7bdd80845897fcab007573f4f374e33
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-07-02 00:51:49 -0400

    bug eventos

--- web/src/app/vinculacion/eventos-y-actividades/page.tsx
+++ web/src/app/vinculacion/eventos-y-actividades/page.tsx
@@ -42,7 +42,7 @@ export default async function Novedades({ searchParams }: PageProps) {
                 />
                 <div className="absolute inset-0 flex items-center justify-center z-10">
                     <h2 className="lg:text-5xl md:text-3xl text-3xl font-bold text-black bg-white/80 px-6 py-2 rounded-md">
-                        Novedades
+                        Eventos y Actividades
                     </h2>
                 </div>
                 <span className="absolute bottom-0 left-0 w-full h-5 bg-sage-green border-y-2 border-black z-10"></span>
